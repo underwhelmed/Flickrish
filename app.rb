b6c0711ca1e84610553fdbd727edc96080f7d39a
Stephen Wright <wright.net@gmail.com>
2012-04-24 22:46:45 -0500

    added handler for nil list (last record)

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'flickraw'
 require 'open-uri'
 require 'FileUtils'
 require 'yaml'
-require 'flickrish'
+require_relative 'flickrish'
 
 keys = YAML.load_file('config.yml')
 FlickRaw.api_key = keys['api_key']
@@ -11,7 +11,7 @@ FlickRaw.shared_secret = keys['secret']
 user_id = keys['user_id']
 backup_dir = keys['directory']
 
-MAX_ATTEMPTS = 10
+MAX_ATTEMPTS = 5
 
 if Dir.exists?(backup_dir)
   FileUtils.rm_rf(backup_dir)
@@ -26,16 +26,21 @@ pages_processed = 1
 while photos_processed < total_photos  
   list = flickr.people.getPublicPhotos :user_id => user_id, :per_page => 500, :page => pages_processed
   500.times do |i|
-    url = Flickrish.getFileUrl(list[i].id, list[i].secret)
-    attempts = 0
-    begin
-      Flickrish.download(url, backup_dir + '/' + File.basename(url))      
-    rescue Exception => ex
-      log.error "Error: #{ex}, cannot download #{url}"
-      attempts = attempts + 1
-      retry if(attempts < MAX_ATTEMPTS)
+    if list != nil
+      url = Flickrish.getFileUrl(list[i].id, list[i].secret)
+      attempts = 0
+      begin
+        Flickrish.download(url, backup_dir + '/' + File.basename(url))      
+      rescue Exception => ex
+        log.error "Error: #{ex}, cannot download #{url}"
+        attempts = attempts + 1
+        sleep(5) # wait 5 seconds before trying again
+        retry if(attempts < MAX_ATTEMPTS)
+      end      
+      photos_processed += 1
+    else
+      break
     end
-    photos_processed += 1
   end
   pages_processed += 1 
 end
